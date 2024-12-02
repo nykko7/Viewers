86a03e98124fc9ce645c1f9fda4c6c5f09e750c4
Nykko <n.perezbus@gmail.com>
2024-12-01 23:46:13 -0300

    feat: load segmentation on mode init

--- extensions/t-chaii/src/getHangingProtocolModule.ts
+++ extensions/t-chaii/src/getHangingProtocolModule.ts
@@ -10,42 +10,21 @@ const defaultProtocol: AppTypes.HangingProtocol.Protocol = {
   editableBy: {},
   protocolMatchingRules: [],
   toolGroupIds: ['default'],
-  // -1 would be used to indicate active only, whereas other values are
-  // the number of required priors referenced - so 0 means active with
-  // 0 or more priors.
   numberOfPriorsReferenced: 0,
-  // Default viewport is used to define the viewport when
-  // additional viewports are added using the layout tool
-  defaultViewport: {
-    viewportOptions: {
-      viewportType: 'stack',
-      toolGroupId: 'default',
-      allowUnmatchedView: true,
-      syncGroups: [
+  displaySetSelectors: {
+    segDisplaySetId: {
+      seriesMatchingRules: [
         {
-          type: 'hydrateseg',
-          id: 'sameFORId',
-          source: true,
-          target: true,
-          options: {
-            matchingRules: ['sameFOR'],
+          weight: 100,
+          attribute: 'Modality',
+          constraint: {
+            equals: 'SEG',
           },
         },
       ],
     },
-    displaySets: [
-      {
-        id: 'defaultDisplaySetId',
-        matchedDisplaySetsIndex: -1,
-      },
-    ],
-  },
-  displaySetSelectors: {
     defaultDisplaySetId: {
-      // Matches displaysets, NOT series
       seriesMatchingRules: [
-        // Try to match series with images by default, to prevent weird display
-        // on SEG/SR containing studies
         {
           weight: 10,
           attribute: 'numImageFrames',
@@ -53,8 +32,6 @@ const defaultProtocol: AppTypes.HangingProtocol.Protocol = {
             greaterThan: { value: 0 },
           },
         },
-        // This display set will select the specified items by preference
-        // It has no affect if nothing is specified in the URL.
         {
           attribute: 'isDisplaySetFromUrl',
           weight: 10,
@@ -81,31 +58,25 @@ const defaultProtocol: AppTypes.HangingProtocol.Protocol = {
             viewportType: 'stack',
             viewportId: 'default',
             toolGroupId: 'default',
-            // This will specify the initial image options index if it matches in the URL
-            // and will otherwise not specify anything.
-            initialImageOptions: {
-              custom: 'sopInstanceLocation',
-            },
-            // Other options for initialImageOptions, which can be included in the default
-            // custom attribute, or can be provided directly.
-            //   index: 180,
-            //   preset: 'middle', // 'first', 'last', 'middle'
-            // },
+            allowUnmatchedView: true,
             syncGroups: [
               {
-                type: 'hydrateseg',
-                id: 'sameFORId',
+                type: 'cameraPosition',
+                id: 'positionSync',
                 source: true,
                 target: true,
-                options: {
-                  matchingRules: ['sameFOR'],
-                },
+              },
+              {
+                type: 'voi',
+                id: 'voiSync',
+                source: true,
+                target: true,
               },
             ],
           },
           displaySets: [
             {
-              id: 'defaultDisplaySetId',
+              id: 'segDisplaySetId',
             },
           ],
         },
